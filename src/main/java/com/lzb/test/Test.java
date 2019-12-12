4033cc3e577db33d9fc8e112f4e4c5bb6294613b
lizhanbin <316892736@qq.com>
2019-12-12 14:00:39 +0800

    Test

--- src/main/java/com/lzb/test/Test.java
+++ src/main/java/com/lzb/test/Test.java
@@ -6,6 +6,8 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.security.MessageDigest;
+
 public class Test {
     public static void main(String []args) {
         int a = 1;
@@ -26,5 +28,40 @@ public class Test {
                     }
                 }).build();
         server.start();
+    }
+}
+
+class Md5Utils {
+    public Md5Utils() {
+    }
+
+    public static String MD5(String param, String salt) {
+        return MD5(param + salt);
+    }
+
+    public static String MD5(String s) {
+        char[] hexDigits = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+        try {
+            byte[] btInput = s.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char[] str = new char[j * 2];
+            int k = 0;
+            byte[] var8 = md;
+            int var9 = md.length;
+
+            for(int var10 = 0; var10 < var9; ++var10) {
+                byte byte0 = var8[var10];
+                str[k++] = hexDigits[byte0 >>> 4 & 15];
+                str[k++] = hexDigits[byte0 & 15];
+            }
+
+            return new String(str);
+        } catch (Exception var12) {
+            return null;
+        }
     }
 }
